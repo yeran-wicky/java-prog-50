15faa1dbcac63d57a8003f793be30ca473ed2497
Yeran Wicky <160687958+yeran-wicky@users.noreply.github.com>
2025-12-29 09:23:50 +0530

    Modified B_2.java

--- src/B_2.java
+++ src/B_2.java
@@ -7,10 +7,10 @@ public class B_2 {
         int max = 0;
         // Create an integer array of size 5
         int[] number = new int[5];
+        Scanner sc = new Scanner(System.in);
         // Loops from 0 to 4 (for the 5 indexes in array)
         for (int i = 0; i < number.length; i++) {
             // Reads the user integer input
-            Scanner sc = new Scanner(System.in);
             System.out.print("Enter a number: ");
             int n = sc.nextInt();
             // If user enters a negative number, it should skip
@@ -25,6 +25,7 @@ public class B_2 {
                 number[i] = n;
             }
         }
+        sc.close();
 
         // Loops through the elements of the array
         for (int j: number){
