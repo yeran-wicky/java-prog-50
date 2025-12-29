f947b4fdd734ac09081a9159ac01c79ebe43dcfe
Yeran Wicky <160687958+yeran-wicky@users.noreply.github.com>
2025-12-29 09:24:58 +0530

    Modified B_1.java

--- src/B_1.java
+++ src/B_1.java
@@ -5,15 +5,16 @@ public class B_1 {
 
         // Create an integer array of size 5
         int[] number = new int[5];
+        Scanner sc = new Scanner(System.in);
         // Loops from 0 to 4 (for the 5 indexes in array)
         for (int i = 0; i < number.length; i++) {
             // Reads the user integer input
-            Scanner sc = new Scanner(System.in);
             System.out.print("Enter a number: ");
             int n = sc.nextInt();
             // Appends the input to the array
             number[i] = n;
         }
+        sc.close();
         // Prints the array using a for loop
         System.out.println();
         for (int num: number){
